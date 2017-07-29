29992ddf89345830d81ece63ef4268225c706d76
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 11:26:50 +0200

    add data-flow from component 1 to component 2 and render the value being passed on

--- src/app/components/level-1/level-1.component.ts
+++ src/app/components/level-1/level-1.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-level-1',
   templateUrl: './level-1.component.html',
-  styleUrls: ['./level-1.component.css']
+  styleUrls: ['./level-1.component.css'],
+  inputs: ['dataAtLvl1']
 })
 export class Level1Component implements OnInit {
+dataAtLvl1 : String;
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
