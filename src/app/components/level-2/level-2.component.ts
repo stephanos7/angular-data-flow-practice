5d55a11c966c9fe7d8b78500a827175498983cff
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 12:12:56 +0200

    add data binding between component-2 and component-1, render data received add component-2

--- src/app/components/level-2/level-2.component.ts
+++ src/app/components/level-2/level-2.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-level-2',
   templateUrl: './level-2.component.html',
-  styleUrls: ['./level-2.component.css']
+  styleUrls: ['./level-2.component.css'],
+  inputs: ['dataAtLvl2']
 })
 export class Level2Component implements OnInit {
+dataAtLvl2: String;
 
   constructor() { }
 
